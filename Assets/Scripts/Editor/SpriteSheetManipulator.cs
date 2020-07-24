d81d97bb08ad7c73fb2703ace4e3a8616a6694d1
Udi <udiberes@gmail.com>
2020-07-24 14:44:59 +0300

    Added spritesheet manipulator. Changed all sprites pivot

---
+++ Assets/Scripts/Editor/SpriteSheetManipulator.cs
@@ -0,0 +1,40 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+namespace Editor
+{
+    public class SpriteSheetManipulator 
+    {
+        [MenuItem("Assets/Change sprite sheet pivot to bottom left", priority = -1)]
+        public static void ChangePivotToBottomLeft()
+        {
+            if(Selection.activeObject == null) return;
+
+            var path = AssetDatabase.GetAssetPath(Selection.activeObject);
+            var textureImporter = (TextureImporter)AssetImporter.GetAtPath(path);
+            
+            if (textureImporter == null)
+            {
+                Debug.Log($"Name: {Selection.activeObject.name} is not {nameof(TextureImporter)}");
+                return;
+            }
+
+            var spriteSheet = new SpriteMetaData[textureImporter.spritesheet.Length];
+
+            for (var i = 0; i < spriteSheet.Length; i++)
+            {
+                var spriteMetaData = textureImporter.spritesheet[i];
+                spriteMetaData.alignment = (int)SpriteAlignment.BottomLeft;
+                spriteMetaData.pivot = new Vector2(0,0);
+                spriteSheet[i] = spriteMetaData;
+            }
+            
+            textureImporter.spritesheet = spriteSheet;
+            
+            if (!AssetDatabase.WriteImportSettingsIfDirty(textureImporter.assetPath)) return;
+            
+            AssetDatabase.ImportAsset(textureImporter.assetPath, ImportAssetOptions.ForceUpdate);
+            AssetDatabase.Refresh();
+        }
+    }
+}
