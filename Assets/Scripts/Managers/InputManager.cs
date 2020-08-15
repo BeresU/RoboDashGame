215865a7d1bae5439643cc76437fa7bbeb8d0828
Udi <udiberes@gmail.com>
2020-08-15 17:27:35 +0300

    WIP movement vie gesture

--- Assets/Scripts/Managers/InputManager.cs
+++ Assets/Scripts/Managers/InputManager.cs
@@ -1,19 +1,31 @@
-﻿using UnityEngine;
+﻿using System;
+using Lean.Touch;
+using UnityEngine;
 
 namespace RoboDash.Managers
 {
     public class InputManager : MonoBehaviour
     {
-        // Start is called before the first frame update
-        void Start()
+        private void Awake()
         {
-        
+            LeanTouch.OnFingerSwipe += OnSwipe;
+            LeanTouch.OnFingerTap += OnTap;
         }
 
-        // Update is called once per frame
-        void Update()
+        private void OnDestroy()
         {
-        
+            LeanTouch.OnFingerSwipe -= OnSwipe;
+            LeanTouch.OnFingerTap -= OnTap;
+        }
+
+        private void OnTap(LeanFinger leanFinger)
+        {
+            Debug.Log($"On Tap; {leanFinger.Tap}");
+        }
+
+        private void OnSwipe(LeanFinger leanFinger)
+        {
+            Debug.Log($"On Swipe; {leanFinger.Swipe}");
         }
     }
 }
