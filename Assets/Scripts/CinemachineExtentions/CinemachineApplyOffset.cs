b6bcc4ad86bfdce701e30c498bc27d9e08c29613
Udi <udiberes@gmail.com>
2020-08-26 23:14:04 +0300

    WIP Cinemachien offset

---
+++ Assets/Scripts/CinemachineExtentions/CinemachineApplyOffset.cs
@@ -0,0 +1,30 @@
+﻿using Cinemachine;
+using UnityEngine;
+
+namespace CinemachineExtentions
+{
+    public class CinemachineApplyOffset : CinemachineExtension
+    {
+        [SerializeField] private Vector3 _offset;
+        [SerializeField] private CinemachineTargetGroup _cinemachineTarget;
+
+        private float _lastOthoSize;
+
+        [Tooltip("When to apply the offset")] [SerializeField]
+        private CinemachineCore.Stage _applyAfter = CinemachineCore.Stage.Aim;
+
+        protected override void PostPipelineStageCallback(CinemachineVirtualCameraBase vcam,
+            CinemachineCore.Stage stage, ref CameraState state, float deltaTime)
+        {
+        //   if (stage != _applyAfter) return;
+        //   var transposer = _cam.GetCinemachineComponent<CinemachineTransposer>();
+        //   
+        //   transposer.m_FollowOffset = _offset;
+        //   var dist = Vector3.Distance(_cinemachineTarget.m_Targets[0].target.position,
+        //       _cinemachineTarget.m_Targets[1].target.position);
+        //   Debug.Log(_cinemachineTarget.BoundingBox.center.x );
+        // var offset = _offset *_cinemachineTarget.BoundingBox.center.x;
+        // state.PositionCorrection = offset;
+        }
+    }
+}
