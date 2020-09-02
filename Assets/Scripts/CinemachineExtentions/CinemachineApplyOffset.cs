e5411c8051f6b4e261ca819cb16aeb7f807368b7
Udi <udiberes@gmail.com>
2020-09-03 00:17:17 +0300

    Finished with camera offset

--- Assets/Scripts/CinemachineExtentions/CinemachineApplyOffset.cs
+++ Assets/Scripts/CinemachineExtentions/CinemachineApplyOffset.cs
@@ -3,26 +3,26 @@ using UnityEngine;
 
 namespace CinemachineExtentions
 {
+    // TODO: support x axis as well
     public class CinemachineApplyOffset : CinemachineExtension
     {
         [SerializeField] private Vector3 _offset;
-        [SerializeField] private CinemachineTargetGroup _cinemachineTarget;
-        [SerializeField] private Vector3 _startingPosition;
-
-        private float _lastOthoSize;
-
+        
         [Tooltip("When to apply the offset")] [SerializeField]
         private CinemachineCore.Stage _applyAfter = CinemachineCore.Stage.Aim;
-
 
         protected override void PostPipelineStageCallback(CinemachineVirtualCameraBase vcam,
             CinemachineCore.Stage stage, ref CameraState state, float deltaTime)
         {
             if (stage != _applyAfter) return;
 
-            var offset = _startingPosition.y + state.Lens.OrthographicSize - _offset.y;
-            
-             state.PositionCorrection += new Vector3(state.FinalPosition.x, offset, state.FinalPosition.z);
+            var offset = GetSizeAccordState(state) - _offset.y;
+
+            state.PositionCorrection += new Vector3(state.PositionCorrection.x, offset, state.PositionCorrection.z);
         }
+        
+        // TODO: need also do for non orthographic camera.
+        private static float GetSizeAccordState(CameraState state) =>
+            state.Lens.Orthographic ? state.Lens.OrthographicSize : 0f;
     }
 }
