3d94220ac7da91a878d5965b5b7001661ffefb29
Udi <udiberes@gmail.com>
2020-10-06 23:59:00 +0300

    GameOver camera on end game

--- Assets/Scripts/Controllers/EndGameController.cs
+++ Assets/Scripts/Controllers/EndGameController.cs
@@ -1,4 +1,6 @@
-﻿using Cinemachine;
+﻿using System;
+using System.Collections;
+using Cinemachine;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
@@ -7,16 +9,40 @@ namespace RoboDash.Controllers
     public class EndGameController : MonoBehaviour
     {
         [SerializeField] private GameObject _ui;
-        [SerializeField] private CinemachineTargetGroup _targetGroup;
-        
+        [SerializeField] private CinemachineVirtualCamera _battleCam;
+        [SerializeField] private CinemachineVirtualCamera _winCam;
+        [SerializeField] private Camera _camera;
+        [SerializeField] private CinemachineBrain _cinemachineBrain;
+
         public void RestartGame() => SceneManager.LoadScene("Battle");
-        
+
         public void OnGameOver(RoboCharacter win, RoboCharacter lose)
         {
+            var orthoSize = _camera.orthographicSize;
+            _battleCam.Follow = null;
+            _battleCam.m_Lens.OrthographicSize = orthoSize;
+            _winCam.Follow = win.transform;
+            _winCam.Priority = _battleCam.Priority + 1;
+            _winCam.m_Transitions.m_OnCameraLive.AddListener(OnCameraLive);
+        }
+
+        private void OnCameraLive(ICinemachineCamera arg0, ICinemachineCamera arg1) =>
+            StartCoroutine(WaitForBlendEnd());
+
+        private IEnumerator WaitForBlendEnd()
+        {
+            _winCam.m_Transitions.m_OnCameraLive.RemoveListener(OnCameraLive);
+            while (_cinemachineBrain.IsBlending && _cinemachineBrain.ActiveBlend.BlendWeight < 0.95f)
+            {
+                yield return null;
+            }
+
+            OnBlendFinished();
+        }
+
+        private void OnBlendFinished()
+        {
             _ui.SetActive(true);
-            var target = _targetGroup.m_Targets[0];
-            target.target = win.transform;
-            _targetGroup.m_Targets = new[] {target };
         }
     }
 }
