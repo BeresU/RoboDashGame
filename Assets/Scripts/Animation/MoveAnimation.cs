f62c7c0846a3d7caab50f3659a87995005eb4252
Udi <udiberes@gmail.com>
2020-10-15 00:02:32 +0300

    Cloud animation

---
+++ Assets/Scripts/Animation/MoveAnimation.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using UnityEngine;
+
+namespace RoboDash.Animation
+{
+    public class MoveAnimation : MonoBehaviour
+    {
+        [SerializeField] private float _speed;
+
+        [SerializeField] private float _maxPos = 2.78f;
+
+        private Vector3 _initPosition;
+
+        private void Awake()
+        {
+            _initPosition = transform.position;
+            _initPosition.x = _maxPos;
+        }
+
+        private void Update()
+        {
+            var pos = transform.position;
+            var newPos = Time.deltaTime * _speed * Vector3.left;
+            pos += newPos;
+            transform.position = pos;
+
+            if (transform.position.x <= -_maxPos)
+            {
+                transform.position = _initPosition;
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/Animation/MoveUpDownAnimation.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+namespace RoboDash.Animation
+{
+    public class MoveUpDownAnimation : MonoBehaviour
+    {
+        [SerializeField] private AnimationCurve _curve;
+        [SerializeField] private float _steps;
+        private float _offset;
+
+        private void Awake()
+        {
+            _curve.postWrapMode = WrapMode.PingPong;
+            _offset = transform.position.y;
+        }
+
+        private void Update()
+        {
+            var curveValue = _curve.Evaluate(Time.time);
+            var pos = transform.position;
+            pos.y = curveValue * _steps + _offset; 
+            transform.position = pos;
+        }
+    }
+}
