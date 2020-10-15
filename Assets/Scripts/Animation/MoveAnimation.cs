18dfd574ea8f57f49d28f7aec017135e9c35d90e
Udi <udiberes@gmail.com>
2020-10-15 20:03:06 +0300

    Finished with clouds

--- Assets/Scripts/Animation/MoveAnimation.cs
+++ Assets/Scripts/Animation/MoveAnimation.cs
@@ -1,20 +1,23 @@
-﻿using System;
-using UnityEngine;
+﻿using UnityEngine;
 
 namespace RoboDash.Animation
 {
     public class MoveAnimation : MonoBehaviour
     {
         [SerializeField] private float _speed;
-
         [SerializeField] private float _maxPos = 2.78f;
-
+        [SerializeField] private bool _randomize;
+        [SerializeField] private float _minSpeed;
+        [SerializeField] private float _maxSpeed;
+        
         private Vector3 _initPosition;
-
+        
         private void Awake()
         {
             _initPosition = transform.position;
             _initPosition.x = _maxPos;
+
+            _speed = _randomize ? Random.Range(_minSpeed, _maxSpeed) : _speed;
         }
 
         private void Update()
